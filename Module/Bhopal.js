f313895bcefeca78da0270c03fe22d5ff9c878ed
Jyoti Vishwakarma <vishwakarmajyoti65@gmail.com>
2025-01-15 21:01:56 +0530

    new

---
+++ Module/Bhopal.js
@@ -0,0 +1,12 @@
+
+const name =" Jyoti";
+const age = 23;
+// const city = "Bhopal";
+
+
+const college = ()=>{
+
+    return "I am from Manit this is my college"; 
+}
+export default college;
+export {name, age,};
